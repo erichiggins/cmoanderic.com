c0566b4b4eb1213f8623e49412c54bf49b8a98b3
Eric Higgins <erichiggins@gmail.com>
2015-12-25 02:25:18 -0800

    Submit current code.

---
+++ js/ga.js
@@ -0,0 +1,11 @@
+var _gaq = [
+  ['_setAccount', 'UA-28040060-1'],
+  ['_trackPageview']
+];
+(function(d, t, p) {
+  var g = d.createElement(t),
+      s = d.getElementsByTagName(t)[0];
+  g.src = p + '//' + ('https:' == p ? 'ssl' : 'www') +
+      '.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g, s);
+})(document, 'script', location.protocol);
